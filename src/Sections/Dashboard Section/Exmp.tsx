96780243f5ce354f57180d96bb4127a7b1c14605
Raghab Neupane <neupaneraghab@gmail.com>
2026-01-09 11:58:58 +0530

    change

--- src/Sections/Dashboard Section/Exmp.tsx
+++ src/Sections/Dashboard Section/Exmp.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react'
 import './Example.css'
 
 const Exmp = () => {
+  const [offset, setOffset] = useState(0)
+
+  useEffect(() => {
+    const onScroll = () => {
+      setOffset(window.scrollY)
+    }
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
   return (
     <div className="page">
         <h3 className='topic'>Dashboard Section</h3>
