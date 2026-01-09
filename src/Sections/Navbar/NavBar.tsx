f29b23d0d870ee872c2482d6747d0f4135fd1d73
Raghab Neupane <neupaneraghab@gmail.com>
2026-01-09 11:12:48 +0530

    change

--- src/Sections/Navbar/NavBar.tsx
+++ src/Sections/Navbar/NavBar.tsx
@@ -3,15 +3,17 @@ import Logo from '../../assets/MEROFINANCE.svg'
 
 const NavBar = () => {
   return (
-    <nav className='navbar-content'>
+    <>
+      <nav className='navbar-content'>
         <img src={Logo} alt='logo' />
         <ul className='Links'>
-            <li><a href='#Services'>Services</a></li>
-            <li><a href='#aboutus'>About Us</a></li>
-            <li><a href='#team'>Team</a></li>
-            <button>Get Started!</button>
+          <li><a href='#Services'>Services</a></li>
+          <li><a href='#aboutus'>About Us</a></li>
+          <li><a href='#team'>Team</a></li>
+          <button>Get Started!</button>
         </ul>
-    </nav>
+      </nav>
+    </>
   )
 }
 
