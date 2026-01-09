d33c1ec107f334b28388fa4ba6fe1a84f01cf3cc
Raghab Neupane <neupaneraghab@gmail.com>
2026-01-09 11:39:00 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from './Sections/Heropage/Hero.tsx'
 import NavBar  from './Sections/Navbar/NavBar.tsx'
+import Exmp from './Sections/Dashboard Section/Exmp.tsx'
 
 const App = () => {
   return (
     <div>
         <NavBar />
         <Hero />
+        <Exmp />
     </div>
   )
 }
---
+++ src/Sections/Dashboard Section/Exmp.tsx
@@ -0,0 +1,11 @@
+import './Example.css'
+
+const Exmp = () => {
+  return (
+    <div className="page">
+        <h3 className='topic'>Dashboard Section</h3>
+    </div>
+  )
+}
+
+export default Exmp
